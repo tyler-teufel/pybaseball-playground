7785a64188398848bb0de685fb493bc9e43dddb4
Tyler Teufel <tdt0025@auburn.edu>
2024-01-24 11:18:22 -0600

    Intro- implemented custom entry.

--- bwar-stats.py
+++ bwar-stats.py
@@ -1,14 +1,14 @@
 from pybaseball import statcast_batter, bwar_bat, playerid_lookup
 
+# Get playerid
+first_name = input('Enter player\'s first name: ')
+last_name = input('Enter player\'s last name: ')
+year = input('Enter year: ')
 # get war stats from baseball reference 
 data = bwar_bat()
-player = data.loc[lambda df: (df['name_common'] == 'Mike Trout') & (df['year_ID'] == 2016)]
+player = data.loc[lambda df: (df['name_common'] == '{first_name} {last_name}'.format(first_name=first_name, last_name=last_name)) & (df['year_ID'] == int(year))]
 
-#.loc[lambda df: df['shield'] == 8]
-#print(data.loc[lambda df: (df['name_common'] == 'Mike Trout') & (df['year_ID'] == 2023)])
+
 print(player[['name_common', 'year_ID', 'WAR']])
-# get war stats plus additional fields from this table 
-data = bwar_bat()
-
 
 
